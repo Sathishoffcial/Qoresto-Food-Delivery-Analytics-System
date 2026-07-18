af9d49c899a28904c36cf5222f68fe45a9a913e4
Sathish A <sathishoff007@gmail.com>
2026-07-18 15:36:41 +0530

    Updated Railway database connection

--- db.py
+++ db.py
@@ -7,5 +7,5 @@ def get_db_connection():
         user=os.getenv("root"),
         password=os.getenv("VknGtXoySpwgSBnrwpklJLMMwIukjgxI"),
         database=os.getenv("railway"),
-        port=int(os.getenv("3306"))
+        port=int(os.getenv("3306", 3306))
     )
